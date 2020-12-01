8ad76eca259a5076beb9f65c73722d7171d4d676
Dmitry Alekseenko <Arohnid@users.noreply.github.com>
2020-12-01 19:45:21 +0300

    Completely changed the program

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int[] drinkPrices = {150, 80, 20, 50};
-        for (int drinkPrice : drinkPrices) {
-            System.out.println(drinkPrice);
-        }
+
+        MyThread thread1 = new MyThread(1);
+        MyThread thread2 = new MyThread(2);
+
+        thread1.start();
+        thread2.start();
     }
 }
