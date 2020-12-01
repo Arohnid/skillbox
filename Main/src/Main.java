3dcfae84ff551615b12a785d3fcbc97914f8f32d
Dmitry Alekseenko <Arohnid@users.noreply.github.com>
2020-12-01 20:50:41 +0300

    Base program

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        MyThread thread1 = new MyThread(1);
-        MyThread thread2 = new MyThread(2);
-
-        thread1.start();
-        thread2.start();
+        String ACCESS_TOKEN = "";
     }
 }
