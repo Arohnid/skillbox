974d5a55af11ea93f9d1340461f89658e5fe78f9
Dmitry Alekseenko <Arohnid@users.noreply.github.com>
2020-12-01 19:31:52 +0300

    Added couple lines of code

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int[] drinkPrices = {150, 80, 20, 50};
-
+        for (int drinkPrice : drinkPrices) {
+            System.out.println(drinkPrice);
+        }
     }
 }
