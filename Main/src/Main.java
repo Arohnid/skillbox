c77f11f543c477193fa0cf2cfe99befae30ba20c
Dmitry Alekseenko <Arohnid@users.noreply.github.com>
2020-12-02 17:49:43 +0300

    Almost final build

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,30 +1,17 @@
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws AWTException, IOException {
+    public static void main(String[] args) {
 
         String ACCESS_TOKEN = "bijmsSa8IHcAAAAAAAAAAfHqpDbDPQ9fUMzr_AekP0AiEdZyGx8zNnQfexT3oO6d";
 
         DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build();
         DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
 
-        BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ImageIO.write(image, "png", baos);
-        byte[]imageInByte = baos.toByteArray();
+        MyThread thread1 = new MyThread(client);
 
-        try {
-            InputStream in = new ByteArrayInputStream(imageInByte);
-            client.files().uploadBuilder("/image3.png")
-                    .uploadAndFinish(in);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+        thread1.start();
     }
 }
--- Main/src/MyThread.java
+++ Main/src/MyThread.java
@@ -1,17 +1,58 @@
 import com.dropbox.core.v2.DbxClientV2;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 
 public class MyThread extends Thread {
 
-    private int threadNumber;
+    private DbxClientV2 client1;
 
     public MyThread(DbxClientV2 client) {
-
+        client1 = client;
     }
 
     @Override
     public void run() {
         for (; ; ) {
-            System.out.println(threadNumber);
+            BufferedImage image = null;
+            try {
+                image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+            } catch (AWTException e) {
+                e.printStackTrace();
+            }
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            try {
+                ImageIO.write(image, "png", baos);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            byte[] imageInByte = baos.toByteArray();
+
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd_HHmmss");
+            Date now = new Date();
+
+            //System.out.println(formatter.format(now));
+
+            try {
+                InputStream in = new ByteArrayInputStream(imageInByte);
+                client1.files().uploadBuilder("/" + formatter.format(now) + ".png")
+                        .uploadAndFinish(in);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
